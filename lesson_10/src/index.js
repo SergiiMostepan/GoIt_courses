4923c35537e800f9e4bffd19c958b3dea56bfd25
Sergii Mostepan <sergiimostepan@gmail.com>
2020-02-23 18:56:38 +0200

    add import

--- lesson_10/src/index.js
+++ lesson_10/src/index.js
@@ -1,3 +1,4 @@
 import './css/normalize.css';
 import './styles.css';
 import './js_components/main';
+import 'basiclightbox/dist/basiclightbox.min.css';
--- lesson_10/src/js_components/main.js
+++ lesson_10/src/js_components/main.js
@@ -1,16 +1,23 @@
 import localStorageLoader from './localStorage_js.js';
 import cardsTemplate from '../templates/card.hbs';
+import modalTemplete from '../templates/modalWindow.hbs';
+import * as basicLightbox from 'basiclightbox';
 
 let cards = [];
 
 const refs = {
   ulList: document.querySelector('.todos'),
-  form: document.querySelector('.form'),
+  btn: document.querySelector('#new_form'),
 };
 
-function createBoxes(inputValue) {
+function createBoxes(inputValue, importance) {
   if (inputValue === '') inputValue = 'there is nothing';
-  cards.push({ title: inputValue, status: false, id: Date.now() + '' });
+  cards.unshift({
+    title: inputValue,
+    status: false,
+    id: Date.now() + '',
+    importance,
+  });
   localStorageLoader.save('cards', cards);
   render(cards);
 }
@@ -26,7 +33,6 @@ function renderLocalStr() {
   cards.push(...upDateData);
   render(cards);
 }
-renderLocalStr();
 
 // make done and delete buttons работает ультра плохо
 
@@ -53,10 +59,46 @@ function changeStatus(id) {
   render(cards);
 }
 
-refs.form.addEventListener('submit', e => {
+// modal is open
+
+function creatBoxesAddivent(e) {
+  const form = document.querySelector('.form');
   e.preventDefault();
-  createBoxes(e.target.elements[0].value);
+
+  const inputs = e.target.elements.raz;
+  let importance;
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      importance = inputs[i].dataset.import;
+    }
+  }
+
+  createBoxes(e.target.elements[0].value, importance);
   e.target.elements[0].value = '';
-});
+  instance.close();
+  form.removeEventListener('submit', creatBoxesAddivent);
+}
+
+const instance = basicLightbox.create(modalTemplete());
+
+function formModalListener() {
+  const form = document.querySelector('.form');
+  // const checkBoxes = document.querySelector('.radio_inputs');
+  // console.dir(checkBoxes);
+
+  // checkBoxes.addEventListener('change', e => console.log(e.target.dataset));
+  form.addEventListener('submit', creatBoxesAddivent);
+}
+
+function modalWindow() {
+  // const instance = basicLightbox.create(modalTemplete());
+  instance.show();
+  formModalListener();
+}
 
 refs.ulList.addEventListener('click', e => actions(e));
+
+refs.btn.addEventListener('click', e => modalWindow(e));
+
+
+window.addEventListener('DOMContentLoaded', renderLocalStr)
