f6cccde9f74a9eb9c3afa37a48af817683377730
Sergii Mostepan <sergiimostepan@gmail.com>
2020-03-19 16:11:31 +0200

    made chat

---
+++ WebSocet/src/js-components/app.js
@@ -0,0 +1,130 @@
+window.URL = window.URL || window.webkitURL;
+const refs = {
+  massege: document.querySelector('#chat'),
+  form: document.querySelector('#inputForm'),
+  asyncMap: document.querySelector('[data-name="async"]'),
+  file: document.querySelector('#file'),
+  userNameForm: document.querySelector('#userNameForm'),
+  chatSection: document.querySelector('.chatSection'),
+  registrationSection: document.querySelector('.registration'),
+  changeDataButton: document.querySelector('#changeData'),
+  avatarImg: document.querySelector('#avatarImg'),
+};
+
+let userName = '';
+let myFile;
+let map;
+let uluru = {};
+
+// получаем текущую геолокацию
+navigator.geolocation.getCurrentPosition(position => {
+  uluru.lat = position.coords.latitude;
+  uluru.lng = position.coords.longitude;
+});
+
+// создаем карту
+(function googleMap() {
+  refs.asyncMap.src =
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyB7Zkwv8ZHein66BAALTpWsJAQorKjiTlw';
+  refs.asyncMap.type = 'text/javascript';
+  refs.asyncMap.onload = () => {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: uluru,
+    });
+  };
+})();
+
+// берем текущее время
+function getDate() {
+  let date = new Date();
+  let h = pad(date.getHours());
+  let m = pad(date.getMinutes());
+  let s = pad(date.getSeconds());
+  return `${h}:${m}:${s}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+// создаем соединение
+let ws = new WebSocket('wss://venify.herokuapp.com/chat');
+
+// выводим инфу с сервака
+ws.onmessage = ({ data }) => {
+  let currentDate = getDate();
+  const { cords, message, name, image } = JSON.parse(data);
+  const newMessage = document.createElement('li');
+  newMessage.textContent = currentDate + ':   ' + name + ': ' + message;
+  refs.massege.appendChild(newMessage);
+  createImage(image, refs.massege);
+  const marker = new google.maps.Marker({
+    position: cords,
+    map: map,
+  });
+};
+
+// создаем елемент фото
+function createImage(src, element) {
+  const image = document.createElement('img');
+  image.classList.add('img');
+  image.src = src;
+  element.appendChild(image);
+}
+
+refs.file.addEventListener('change', e => {
+  const file = e.target.files[0];
+  const FR = new FileReader();
+  FR.readAsDataURL(file);
+  FR.addEventListener('load', function(e) {
+    myFile = e.target.result;
+    avatarImg.src = myFile;
+    // createImage(myFile, refs.registrationSection);
+  });
+});
+
+// отправляем данные на сервер
+const sendData = message => {
+  ws.send(
+    JSON.stringify({
+      cords: uluru,
+      message: message,
+      name: userName,
+      image: myFile,
+    }),
+  );
+};
+
+// вводим имя
+function newName(inputName) {
+  // if (userName === '')
+  userName = inputName;
+}
+function visibleChat() {
+  refs.chatSection.classList.add('visible');
+  refs.registrationSection.classList.add('invisible');
+}
+
+function changeData() {
+  refs.registrationSection.classList.toggle('invisible');
+  refs.chatSection.classList.toggle('visible');
+}
+
+refs.userNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  newName(e.target.elements.userName.value);
+  e.target.elements.userName.value = '';
+  visibleChat();
+});
+
+refs.changeDataButton.addEventListener('click', e => {
+  e.preventDefault();
+  changeData();
+});
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  sendData(e.target.elements.inputField.value);
+  e.target.elements.inputField.value = '';
+});
